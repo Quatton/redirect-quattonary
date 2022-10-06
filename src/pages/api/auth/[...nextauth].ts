2ad57dc20073fa50d392c3939cdccef1f0da6fce
Quatton <atomicatives@gmail.com>
2022-10-06 22:33:00 +0900

    added prisma adapter

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,11 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
+import { prisma } from "../../../db/client";
+
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID || "",
