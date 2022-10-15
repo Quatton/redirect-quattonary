19964332bc551ed0719df89920b245fe067ad7a6
Quatton <atomicatives@gmail.com>
2022-10-16 06:41:12 +0900

    updated middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,15 +5,15 @@ export async function middleware(req: NextRequest, ev: NextFetchEvent) {
 
   const slugFetch = await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`);
   if (slugFetch.status === 404) {
-    return NextResponse.redirect(req.nextUrl.origin);
+    return NextResponse.next();
   }
   const data = await slugFetch.json();
 
   if (data?.url) {
-    return NextResponse.redirect(data.url);
+    return NextResponse.redirect(new URL("", data.url));
   }
 }
 
 export const config = {
-  matcher: "/:slug",
+  matcher: "/((?!api|static|favicon.ico).*)",
 };
