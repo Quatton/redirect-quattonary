3dccee9796ab37473fa3e80175d65288640a7b64
Quatton <atomicatives@gmail.com>
2022-10-16 06:41:50 +0900

    moved create new link to /admin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,9 @@
 import type { NextPage } from "next";
-import dynamic from "next/dynamic";
-import { Suspense } from "react";
-
-const CreateLinkForm = dynamic(() => import("../components/create-link"), {
-  ssr: false,
-});
 
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-gray-950 text-white">
-      <Suspense>
-        <CreateLinkForm />
-      </Suspense>
+      Welcome
     </div>
   );
 };
