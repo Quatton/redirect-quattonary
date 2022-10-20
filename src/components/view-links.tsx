40f858b720de7944395d5c257dee7871744d4920
Quatton <atomicatives@gmail.com>
2022-10-20 19:24:43 +0900

    overflow issue

--- src/components/view-links.tsx
+++ src/components/view-links.tsx
@@ -94,109 +94,111 @@ const ViewLinks: React.FC = () => {
       <div className="mb-4 text-3xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-transparent font-bold">
         {origin}/...
       </div>
-      <table className="w-full rounded-md border-spacing-0 border-separate shadow-md">
-        <thead>
-          <tr className="header">
-            <th className="rounded-tl-md">ID</th>
-            <th>SLUG</th>
-            <th>URL</th>
-            <th className="rounded-tr-md">ACTIONS</th>
-          </tr>
-        </thead>
-        <tbody>
-          <>
-            <tr>
-              <td></td>
-              <td>
-                <input
-                  type="text"
-                  className={slugInput}
-                  value={createForm.slug}
-                  onChange={(e) => {
-                    if (e.target.value) debounce(slugCheck.refetch, 100);
-                    setCreateForm({ ...createForm, slug: e.target.value });
-                  }}
-                />
-              </td>
-              <td>
-                <input
-                  type="url"
-                  value={createForm.url}
-                  onChange={(e) => {
-                    setCreateForm({ ...createForm, url: e.target.value });
-                  }}
-                />
-              </td>
-              <td>
-                <button
-                  onClick={() => createFormSubmit()}
-                  className="
-                  bg-pink-500 px-4 py-2 rounded-md 
-                  shadow-md hover:bg-pink-400 [&:not(:disabled)]:hover:-translate-y-1 transition-all 
-                  disabled:bg-pink-800 disabled:text-gray-700"
-                  disabled={slugCheck.isFetched && slugCheck.data!.used}
-                >
-                  Create
-                </button>
-              </td>
+      <div className="max-w-full overflow-x-auto">
+        <table className="max-w-full rounded-md border-spacing-0 border-separate shadow-md table-fixed">
+          <thead>
+            <tr className="header">
+              <th className="rounded-tl-md">ID</th>
+              <th>SLUG</th>
+              <th>URL</th>
+              <th className="rounded-tr-md">ACTIONS</th>
             </tr>
-            {shortlinks.map((shortlink) => (
-              <tr className="even:bg-neutral-800" key={shortlink.id}>
-                <td>{shortlink.id}</td>
+          </thead>
+          <tbody>
+            <>
+              <tr>
+                <td></td>
                 <td>
-                  <div className="flex items-center [&:hover_button]:block [&_button]:hidden">
-                    <input
-                      onFocus={() => updateFormInput.onFocus(shortlink)}
-                      onBlur={() => updateFormInput.onBlur(shortlink)}
-                      type="text"
-                      value={
-                        updateForm.id === shortlink.id
-                          ? updateForm.slug
-                          : shortlink.slug
-                      }
-                      onChange={(e) =>
-                        updateForm.id === shortlink.id &&
-                        setUpdateForm({ ...updateForm, slug: e.target.value })
-                      }
-                    />
-                    <button
-                      className="absolute bg-neutral-600 rounded-md 
-                    hover:bg-neutral-500 right-9 px-2 py-1 transition-all"
-                      onClick={() => copy(`${origin}/${shortlink.slug}`)}
-                    >
-                      Copy
-                    </button>
-                  </div>
+                  <input
+                    type="text"
+                    className={slugInput}
+                    value={createForm.slug}
+                    onChange={(e) => {
+                      if (e.target.value) debounce(slugCheck.refetch, 100);
+                      setCreateForm({ ...createForm, slug: e.target.value });
+                    }}
+                  />
                 </td>
                 <td>
                   <input
-                    onFocus={() => updateFormInput.onFocus(shortlink)}
-                    onBlur={() => updateFormInput.onBlur(shortlink)}
                     type="url"
-                    value={
-                      updateForm.id === shortlink.id
-                        ? updateForm.url
-                        : shortlink.url
-                    }
-                    onChange={(e) =>
-                      updateForm.id === shortlink.id &&
-                      setUpdateForm({ ...updateForm, url: e.target.value })
-                    }
+                    value={createForm.url}
+                    onChange={(e) => {
+                      setCreateForm({ ...createForm, url: e.target.value });
+                    }}
                   />
                 </td>
                 <td>
                   <button
-                    className="text-red-500 hover:underline underline-offset-2"
-                    onClick={(e) => deleteSlug.mutate({ id: shortlink.id })}
+                    onClick={() => createFormSubmit()}
+                    className="
+                    bg-pink-500 px-4 py-2 rounded-md
+                    shadow-md hover:bg-pink-400 [&:not(:disabled)]:hover:-translate-y-1 transition-all
+                    disabled:bg-pink-800 disabled:text-gray-700"
+                    disabled={slugCheck.isFetched && slugCheck.data!.used}
                   >
-                    Delete
+                    Create
                   </button>
                 </td>
               </tr>
-            ))}
-          </>
-        </tbody>
-      </table>
+              {shortlinks.map((shortlink) => (
+                <tr className="even:bg-neutral-800" key={shortlink.id}>
+                  <td>{shortlink.id}</td>
+                  <td>
+                    <div className="relative flex items-center  [&:hover_button]:block [&_button]:hidden">
+                      <input
+                        onFocus={() => updateFormInput.onFocus(shortlink)}
+                        onBlur={() => updateFormInput.onBlur(shortlink)}
+                        type="text"
+                        value={
+                          updateForm.id === shortlink.id
+                            ? updateForm.slug
+                            : shortlink.slug
+                        }
+                        onChange={(e) =>
+                          updateForm.id === shortlink.id &&
+                          setUpdateForm({ ...updateForm, slug: e.target.value })
+                        }
+                      />
+                      <button
+                        className="absolute bg-neutral-600 rounded-md
+                      hover:bg-neutral-500 right-1 px-2 py-1 transition-all"
+                        onClick={() => copy(`${origin}/${shortlink.slug}`)}
+                      >
+                        Copy
+                      </button>
+                    </div>
+                  </td>
+                  <td>
+                    <input
+                      onFocus={() => updateFormInput.onFocus(shortlink)}
+                      onBlur={() => updateFormInput.onBlur(shortlink)}
+                      type="url"
+                      value={
+                        updateForm.id === shortlink.id
+                          ? updateForm.url
+                          : shortlink.url
+                      }
+                      onChange={(e) =>
+                        updateForm.id === shortlink.id &&
+                        setUpdateForm({ ...updateForm, url: e.target.value })
+                      }
+                    />
+                  </td>
+                  <td>
+                    <button
+                      className="text-red-500 hover:underline underline-offset-2"
+                      onClick={(e) => deleteSlug.mutate({ id: shortlink.id })}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
