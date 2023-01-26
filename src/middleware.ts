d0b04b0ecd1c6f4f144e58fe09bd52671fe61465
Quatton <atomicatives@gmail.com>
2023-01-27 01:58:01 +0900

    excluded admin path

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,5 +15,5 @@ export async function middleware(req: NextRequest, ev: NextFetchEvent) {
 }
 
 export const config = {
-  matcher: "/((?!api|static|_next).*)",
+  matcher: "/((?!api|static|_next|admin).*)",
 };
